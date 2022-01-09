2f60c417df45c2f477a178b199cd2df4c9d72848
Martina Terlevic <martina.terlevic@thoughtworks.com>
2022-01-09 23:14:17 +0100

    [FEAT] Add permissions; only owner of post can edit or delete it

--- blog/api/views.py
+++ blog/api/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics
 from api import serializers
 from django.contrib.auth.models import User
 from api.models import Post
+from rest_framework import permissions
+from api.permissions import IsOwnerOrReadOnly
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -15,6 +17,8 @@ class UserDetail(generics.RetrieveAPIView):
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -22,3 +26,5 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
