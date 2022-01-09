8d3534a1aeb4e5194e4dc873372793c1c03b37ae
Martina Terlevic <martina.terlevic@thoughtworks.com>
2022-01-09 23:07:04 +0100

    [FEAT] Add User and Post

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from rest_framework import generics
+from api import serializers
+from django.contrib.auth.models import User
+from api.models import Post
 
-# Create your views here.
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
