4177fad8f0bddd0656bbc90d9e557fb591131ade
Martina Terlevic <martina.terlevic@thoughtworks.com>
2022-01-30 12:35:16 +0100

    [FEAT] Added React-bootstrap, removed tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
 import { Homepage } from './Homepage/Homepage';
+import { Navbar } from './Navbar/Navbar';
 
-export const App = (): JSX.Element => ( 
+export const App = (): JSX.Element => (
+  <>
+  <Navbar />
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Homepage />} />
     </Routes>
   </BrowserRouter>
+  </>
 );
--- frontend/src/ErrorHandling/ErrorPage.tsx
+++ frontend/src/ErrorHandling/ErrorPage.tsx
@@ -1,13 +1,17 @@
+import { ReactNode } from "react";
+import React from "react"
 import { Link } from "react-router-dom"
 
-const ErrorPage = () => {
-    return ( 
-        <div className="not-found">
-            <h2>404</h2>
-            <p>The requested page was not found</p>
-            <Link to='/'>Back</Link>
-        </div>
-     );
+class ErrorPage extends React.Component {
+    render(): ReactNode {
+        return ( 
+            <div className="not-found">
+                <h2>404</h2>
+                <p>The requested page was not found</p>
+                <Link to='/'>Back</Link>
+            </div>
+         );
+    }
 }
 
 export default ErrorPage;
