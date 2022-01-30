c3e9dd7c7aa86c069278451cb1f5cf41058c3ecc
Martina Terlevic <terlevic.martina@gmail.com>
2022-01-30 17:27:24 +0100

    [FEATURE] Add template for input form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
+import { BrowserRouter, Route, Routes} from 'react-router-dom'; 
 import { Homepage } from './Homepage/Homepage';
 import { PostList } from './Post/PostList'
 import { Navbar } from './Navbar/Navbar';
+import { CreatePost } from './Post/CreatePost'
 
 export const App = (): JSX.Element => (
   <>
@@ -10,6 +11,7 @@ export const App = (): JSX.Element => (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/posts" element = { <PostList />} />
+      <Route path="/create" element = { <CreatePost />} />
     </Routes>
   </BrowserRouter>
   </>
--- frontend/src/Post/PostDetails.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-class PostDetails extends React.Component {
-}
-
-export default PostDetails;
